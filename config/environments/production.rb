e12ffd63c6902ae7a220db9e05b8583de47bb567
esilvac <esilva1995@gmail.com>
2017-12-16 17:48:28 -0500

    update for new heroku domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://pure-springs-45975.herokuapp.com',
+    :domain => 'https://esilvac.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
